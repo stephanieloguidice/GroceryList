6a02c7128bcc6802bd47bfe221184142133cc888
Stephanie Loguidice <36975622+stephanieloguidice@users.noreply.github.com>
2019-02-13 14:46:05 -0500

    First Try at this after my lesson with Michelle.
    I tried using classes that Michelle taught me yesterday, but I could not get it to work for the life of me with this file even with using the debugger. I tried solving the problem another way, but that also didn't work. So I must research more into this. It made sense while she was showing me, but it is a lot harder to put into practice! I'll try again, later after work! This is fun though!

---
+++ grocery_list.py
@@ -0,0 +1,43 @@
+# Make a program that asks for a grocery list
+# asks for the department
+# would like it to list and save all of those items in a list
+# print out that list
+# Have a menu to organize it!
+
+grocery_list = []
+
+print("Welcome to your Virtual Grocery List!")
+input("Please type Add, List, or End:")
+
+if input == "Add" or "add":
+    pass
+    # fill this in later
+elif input == "List" or "list":
+    pass
+    # fill this in later
+elif input == "End" or "end":
+    pass
+    # fill this in later
+else:
+    print("You seem to have headbutted your keyboard"
+          ", please try again.")
+
+
+# Adding items and departments to the program
+class GroceryList:
+    def__init__(self, grocery_item, department):
+
+
+# def grocery():
+#     grocery_item = input("Please input your grocery item:")
+#     department = input("Please input the department:")
+#     grocery_item.append(grocery_list)
+#     department.append(grocery_list)
+#
+#
+# print("Item: " + grocery_item + "Department: "
+#       + department)
+
+# listing the items
+
+# quitting
