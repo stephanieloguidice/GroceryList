997062e11b8f046acecb38c4e35286798ea23b98
Stephanie Loguidice <36975622+stephanieloguidice@users.noreply.github.com>
2019-02-18 16:41:53 -0500

    progress to the grocery list
    need to practice more classes and loops

--- grocery_list.py
+++ grocery_list.py
@@ -3,41 +3,75 @@
 # would like it to list and save all of those items in a list
 # print out that list
 # Have a menu to organize it!
-
 grocery_list = []
 
+##functions
+
+
+def add_grocery():
+    grocery_item = input(f"Please input your Grocery Item:")
+    department = input(f"Please input the Department:")
+    grocery_list.append(grocery_item)
+    grocery_list.append(department)
+    print("Item: " + grocery_item + " Department: " + department)
+    return grocery_list
+
+
+def return_menu():
+    print("Welcome to your Virtual Grocery List!")
+    answer = input("Please type Add, List, or End:")
+
+    while True:
+        if answer == "Add" or "add":
+            add_grocery()
+            return_menu()
+
+        elif answer == "List" or "list":
+            list_items()
+
+        elif answer == "End" or "end":
+            print("Thank you for using the Grocery List App!")
+            exit()
+        else:
+            print("You seem to have headbutted your keyboard"
+                  ", please try again.")
+
+
+
 print("Welcome to your Virtual Grocery List!")
-input("Please type Add, List, or End:")
+answer = input("Please type Add, List, or End:")
 
-if input == "Add" or "add":
-    pass
-    # fill this in later
-elif input == "List" or "list":
-    pass
-    # fill this in later
-elif input == "End" or "end":
-    pass
-    # fill this in later
-else:
-    print("You seem to have headbutted your keyboard"
-          ", please try again.")
+while True:
+    if answer == "Add" or "add":
+        add_grocery()
+        return_menu()
 
+    elif answer == "List" or "list":
+        print(grocery_list)
 
-# Adding items and departments to the program
-class GroceryList:
-    def__init__(self, grocery_item, department):
+    elif answer == "End" or "end":
+        print("Thank you for using the Grocery List App!")
+        exit()
+    else:
+        print("You seem to have headbutted your keyboard"
+              ", please try again.")
 
 
-# def grocery():
-#     grocery_item = input("Please input your grocery item:")
-#     department = input("Please input the department:")
-#     grocery_item.append(grocery_list)
-#     department.append(grocery_list)
+# # Adding items and departments to the program
 #
 #
-# print("Item: " + grocery_item + "Department: "
-#       + department)
-
-# listing the items
-
-# quitting
+#
+#
+# # def grocery():
+# #     grocery_item = input("Please input your grocery item:")
+# #     department = input("Please input the department:")
+# #     grocery_item.append(grocery_list)
+# #     department.append(grocery_list)
+# #
+# #
+# # print("Item: " + grocery_item + "Department: "
+# #       + department)
+#
+# # listing the items
+#
+# # quitting
